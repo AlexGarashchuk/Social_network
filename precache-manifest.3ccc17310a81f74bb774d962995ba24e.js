db4e79459f64ad9d5bf314b3367fbfce52b4245a
Alex <algaraschukicloud.com@Alex.local>
2019-05-15 14:13:20 +0300

    Updates

---
+++ precache-manifest.3ccc17310a81f74bb774d962995ba24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc28fdae392293a28dfbb006c095e8e",
+    "url": "/github.io/test/index.html"
+  },
+  {
+    "revision": "f17599b5d42326624d9f",
+    "url": "/github.io/test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/github.io/test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f17599b5d42326624d9f",
+    "url": "/github.io/test/static/js/main.c9f92686.chunk.js"
+  },
+  {
+    "revision": "4844bbbfae99a36684bf",
+    "url": "/github.io/test/static/js/runtime~main.f7c9f722.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6c4933f0496c8c1580ac312b6c60b1e.js"
+  "/github.io/test/precache-manifest.3ccc17310a81f74bb774d962995ba24e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
