9a30283056b6d7cea68d025e7d5b5400cfb97ac9
Alex <algaraschukicloud.com@Alex.local>
2019-05-15 19:05:38 +0300

    Updates

---
+++ precache-manifest.85fa1f6734f54ec03ef3bb78f80d5d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "515b0bd24ffc816339a8",
+    "url": "/network/static/js/runtime~main.515b0bd2.js"
+  },
+  {
+    "revision": "a7984baba705e4ab5d91",
+    "url": "/network/static/js/main.a7984bab.chunk.js"
+  },
+  {
+    "revision": "2a896ceb74b802023823",
+    "url": "/network/static/js/2.2a896ceb.chunk.js"
+  },
+  {
+    "revision": "a7984baba705e4ab5d91",
+    "url": "/network/static/css/main.58b07ae6.chunk.css"
+  },
+  {
+    "revision": "2a896ceb74b802023823",
+    "url": "/network/static/css/2.abeef3d2.chunk.css"
+  },
+  {
+    "revision": "f9cb9037b2d3090378004442f0712e19",
+    "url": "/network/index.html"
+  }
+];
